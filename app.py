a482bea455983a04824618fdc9b82db7a0a1dd25
Dimple <dimple.nag.20@gmail.com>
2026-06-03 23:22:41 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-import os
 
 st.set_page_config(
     page_title="Luxury Fashion Analytics",
@@ -11,23 +10,13 @@ st.set_page_config(
 
 @st.cache_data
 def load_data():
-
-    st.write("Current Directory:", os.getcwd())
-
-    st.write("Root Files:", os.listdir("."))
-
-    if os.path.exists("data"):
-        st.write("Data Folder Files:", os.listdir("data"))
-    else:
-        st.error("data folder not found")
-
     return pd.read_csv("data/Luxury_Products_Apparel_Data.csv")
 
 df = load_data()
 
 st.title("💎 Luxury Fashion Analytics Dashboard")
 
-col1,col2,col3,col4 = st.columns(4)
+col1, col2, col3, col4 = st.columns(4)
 
 col1.metric("Products", len(df))
 col2.metric("Categories", df["Category"].nunique())
@@ -39,10 +28,10 @@ st.divider()
 category_counts = df["Category"].value_counts()
 
 fig = px.bar(
-    category_counts,
     x=category_counts.index,
     y=category_counts.values,
+    labels={"x": "Category", "y": "Count"},
     title="Category Distribution"
 )
 
-st.plotly_chart(fig,use_container_width=True)
+st.plotly_chart(fig, use_container_width=True)
